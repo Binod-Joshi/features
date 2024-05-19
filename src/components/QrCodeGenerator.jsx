332c78911f5dc31b10106cae58049c9ba6358b8c
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-05-19 14:31:31 +0530

    Qrcode functionality comp

--- src/components/QrCodeGenerator.jsx
+++ src/components/QrCodeGenerator.jsx
@@ -1,20 +1,46 @@
-import React, { useState } from 'react'
+import QRCode from 'qrcode'
+import { useState } from 'react'
 
 const QrCodeGenerator = () => {
-    const [url, setUrl] = useState("");
+  const [url, setUrl] = useState('')
+	const [qr, setQr] = useState('')
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-      <input type="url" placeholder='Enter a url to generate qr code' onChange={(e) => setUrl(e.target.value)} />
-      <button type='submit'> Generate QR Code</button>
-      </form>
-      <img src={`https://chart.googleapis.com/chart?cht=qr&chs=180*180&chl=https://www.playerofcode.in`} alt="QR Code" />
-    </div>
-  )
+	const GenerateQRCode = () => {
+		QRCode.toDataURL(url, {
+			width: 400,
+			margin: 2,
+			color: {
+				dark: '#335383FF',
+				light: '#EEEEEEFF'
+			}
+		}, (err, url) => {
+			if (err) return console.error(err)
+
+			console.log(url)
+			setQr(url)
+		})
+	}
+
+	return (
+		<div className="app">
+			<h1>QR Generator</h1>
+			<div style={{display:'flex',alignItems:"center",justifyContent:"center",gap:"10px",marginBottom:"20px"}}>
+      <input 
+				type="text"
+				placeholder="e.g. https://google.com"
+				value={url}
+				onChange={e => setUrl(e.target.value)}
+        style={{height:"30px"}} />
+			<button onClick={GenerateQRCode}>Generate</button>
+      </div>
+			{qr && <>
+				<div style={{display:'flex',alignItems:"center",justifyContent:"center",flexDirection:"column",gap:"10px"}}>
+        <img src={qr} />
+				<a href={qr} download="qrcode.png">Download</a>
+        </div>
+			</>}
+		</div>
+	)
 }
 
 export default QrCodeGenerator
