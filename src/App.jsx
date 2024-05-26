f7b8c1261daebf2523177b16b06625363f08e60c
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-05-26 11:06:15 +0530

    quiz app functionality is added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import QrCodeGenerator from './components/QrCodeGenerator'
+import Quiz from './components/quiz/Quiz'
 
 function App() {
 
   return (
     <>
-      <QrCodeGenerator/>
+      {/* <QrCodeGenerator/> */}
+      <Quiz/>
     </>
   )
 }
---
+++ src/utils/quizData/QuizData.js
@@ -0,0 +1,52 @@
+export const QuizData = [
+    {
+        question: "What is React?",
+        options: ["A Js library for building user interfaces", "A database management system", "A CSS framework", "A web server"],
+        answer: 1
+    },
+    {
+        question: "Who developed React?",
+        options: ["Google", "Facebook", "Microsoft", "Twitter"],
+        answer: 2
+    },
+    {
+        question: "What is a React component?",
+        options: ["A reusable piece of UI", "A database table", "A CSS class", "A JavaScript function"],
+        answer: 1
+    },
+    {
+        question: "What is JSX?",
+        options: ["A type of JavaScript", "A syntax extension for JavaScript", "A database query language", "A CSS preprocessor"],
+        answer: 2
+    },
+    {
+        question: "What is the use of useState in React?",
+        options: ["To manage the state in a functional component", "To fetch data from an API", "To style components", "To handle form submissions"],
+        answer: 1
+    },
+    {
+        question: "What is a React hook?",
+        options: ["A function that lets you use state and other React features", "A method to connect to databases", "A function for handling events", "A type of CSS rule"],
+        answer: 1
+    },
+    {
+        question: "Which method is used to update the state in a React class component?",
+        options: ["updateState", "changeState", "setState", "modifyState"],
+        answer: 3
+    },
+    {
+        question: "What is the virtual DOM?",
+        options: ["A programming concept to manipulate real DOM directly", "A lightweight copy of the real DOM", "A new feature in JavaScript", "A type of CSS"],
+        answer: 2
+    },
+    {
+        question: "Life cycle method in React?",
+        options: ["componentDidMount", "initialize", "start", "begin"],
+        answer: 1
+    },
+    {
+        question: "What is the purpose of props in React?",
+        options: ["To pass data between components", "To style components", "To manage state", "To connect to APIs"],
+        answer: 1
+    }
+];
